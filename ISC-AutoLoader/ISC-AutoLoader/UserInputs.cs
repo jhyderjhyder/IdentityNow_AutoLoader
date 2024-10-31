346f4d33ee5383a954cfd3fb8686f553164f81d7
Jed <ctdljh@gmail.com>
2024-10-30 19:33:45 -0500

    First Draft

---
+++ ISC-AutoLoader/ISC-AutoLoader/UserInputs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISC_AutoLoader
+{
+    public class UserInputs
+    {
+        public UserInputs() { }
+
+        private String url = Environment.GetEnvironmentVariable("ISC_URL");
+        private String client_id = Environment.GetEnvironmentVariable("ISC_CLIENT_ID");
+        private String client_secret = Environment.GetEnvironmentVariable("ISC_CLIENT_SECRET");
+        private String startingFolder = Environment.GetEnvironmentVariable("ISC_FOLDER");
+
+        //TODO extra validation
+        public String getUrl(){return "https://" + url;}
+        public String getClientId() { return client_id;}
+        public String getClientSecret() { return client_secret;}
+        public String getStartingFolder() {  return startingFolder;}
+    }
+}
